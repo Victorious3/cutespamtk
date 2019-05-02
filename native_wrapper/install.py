67ed25f83e4ed70d68d74c780e321f58371eadd8
Victorious3 <vic@nightfall.moe>
2019-05-02 20:52:25 +0200

    Make installer work on linux

--- native_wrapper/install.py
+++ native_wrapper/install.py
@@ -1,4 +1,6 @@
-import sys, os, ctypes
+#!/usr/bin/env python3
+
+import sys, os, ctypes, json, contextlib
 from pathlib import Path
 
 EXTENSION = "moe.nightfall.booru"
@@ -6,6 +8,11 @@ DIR = Path(__file__).resolve().parent
 
 REG_CHROME = os.path.join(r"Software\Google\Chrome\NativeMessagingHosts", EXTENSION)
 REG_FF = os.path.join(r"Software\Mozilla\NativeMessagingHosts", EXTENSION)
+
+PATH_CHROME_SYSTEM = Path("/etc/opt/chrome/native-messaging-hosts")
+PATH_CHROMIUM_SYSTEM = Path("/etc/chromium/NativeMessagingHosts")
+PATH_CHROME_USER = Path("~/.config/google-chrome/NativeMessagingHosts").expanduser()
+PATH_CHROMIUM_USER = Path("~/.config/chromium/NativeMessagingHosts").expanduser()
 
 if os.name == "nt":
     IS_ROOT = ctypes.windll.shell32.IsUserAnAdmin() != 0
@@ -26,6 +33,34 @@ def install():
 
         winreg.CloseKey(KEY_CHROME)
         winreg.CloseKey(KEY_FF)
+    elif os.name == "posix":
+        with open(Path(__file__).parent / "chrome" / (EXTENSION + ".json"), "r") as chrf:
+            manifest_chrome = json.load(chrf)
+        with open(Path(__file__).parent / "firefox" / (EXTENSION + ".json"), "r") as fff:
+            manifest_ff = json.load(fff)
+
+        native_wrapper = (Path(__file__).parent / "native-wrapper.sh").resolve().absolute()
+
+        manifest_chrome["path"] = str(native_wrapper)
+        manifest_ff["path"] = str(native_wrapper)
+
+        if IS_ROOT:
+            if PATH_CHROME_SYSTEM.parent.exists(): 
+                PATH_CHROME_SYSTEM.mkdir(exist_ok = True)
+                with open(PATH_CHROME_SYSTEM / (EXTENSION + ".json"), "w") as chrf: json.dump(manifest_chrome, chrf)
+            if PATH_CHROMIUM_SYSTEM.parent.exists():
+                PATH_CHROMIUM_SYSTEM.mkdir(exist_ok = True)
+                with open(PATH_CHROMIUM_SYSTEM / (EXTENSION + ".json"), "w") as chrf: json.dump(manifest_chrome, chrf)
+        else:
+            if PATH_CHROME_USER.parent.exists():
+                PATH_CHROME_USER.mkdir(exist_ok = True)
+                with open(PATH_CHROME_USER / (EXTENSION + ".json"), "w") as chrf: json.dump(manifest_chrome, chrf)
+            if PATH_CHROMIUM_USER.parent.exists():
+                PATH_CHROMIUM_USER.mkdir(exist_ok = True)
+                with open(PATH_CHROMIUM_USER / (EXTENSION + ".json"), "w") as chrf: json.dump(manifest_chrome, chrf)
+
+
+
     print("Successfully installed cutespam")
 
 def remove():
@@ -35,14 +70,28 @@ def remove():
         ROOT_KEY = winreg.HKEY_LOCAL_MACHINE if IS_ROOT else winreg.HKEY_CURRENT_USER
         winreg.DeleteKey(ROOT_KEY, REG_CHROME)
         winreg.DeleteKey(ROOT_KEY, REG_FF)
+    elif os.name == "posix":
+        if IS_ROOT:
+            with contextlib.suppress(FileNotFoundError): (PATH_CHROME_SYSTEM / (EXTENSION + ".json")).unlink()
+            with contextlib.suppress(FileNotFoundError): (PATH_CHROMIUM_SYSTEM / (EXTENSION + ".json")).unlink()
+        else:
+            with contextlib.suppress(FileNotFoundError): (PATH_CHROME_USER / (EXTENSION + ".json")).unlink()
+            with contextlib.suppress(FileNotFoundError): (PATH_CHROMIUM_USER / (EXTENSION + ".json")).unlink()
+
     print("Successfully removed cutespam")
 
 if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Specify -install or -remove")
+        sys.exit(-1)
     try:
         if sys.argv[1] == "-install":
             install()
         elif sys.argv[1] == "-remove":
             remove()
+        else:
+            print("Specify -install or -remove")
+            sys.exit(-1)
     except Exception as e:
         print("An error has occured during installation:", str(e))
-        sys.exit(-1)
+        raise e
