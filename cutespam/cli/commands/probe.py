ccafe194b8f852ec95613d299c377cae242d35a7
Victorious3 <vic@nightfall.moe>
2019-05-08 18:50:00 +0200

    Make probe read from stdin as well

--- cutespam/cli/commands/probe.py
+++ cutespam/cli/commands/probe.py
@@ -4,31 +4,37 @@ from cutespam.cli import UUIDFileCompleter
 DESCRIPTION = "Outputs a file's tags"
 
 def main(ARGS):
+    import sys
+
     from uuid import UUID
     from pathlib import Path
 
     from cutespam import db
     from cutespam.meta import CuteMeta
 
-    fp = Path(ARGS.file)
-    if fp.exists() and fp.is_file():
-        cute_meta = CuteMeta.from_file(fp)
-    else:
-        try: 
-            uid = UUID(ARGS.file)
-            cute_meta = CuteMeta.from_db(uid)
-        except:
-            print("\n".join(str(uid) for uid in db.get_tab_complete_uids(ARGS.file)))
-            return
+    if not ARGS.file and not sys.stdin.isatty():
+        ARGS.file = sys.stdin.read().splitlines()
 
-    if ARGS.json:
-        print(cute_meta.to_json(ARGS.tag))
-    else:
-        print(cute_meta.to_string(ARGS.tag))
+    for file in ARGS.file:
+        fp = Path(file)
+        if fp.exists() and fp.is_file():
+            cute_meta = CuteMeta.from_file(file)
+        else:
+            try: 
+                uid = UUID(file)
+                cute_meta = CuteMeta.from_db(uid)
+            except:
+                print("\n".join(str(uid) for uid in db.get_tab_complete_uids(file)))
+                return
+
+        if ARGS.json:
+            print(cute_meta.to_json(ARGS.tag))
+        else:
+            print(cute_meta.to_string(ARGS.tag))
 
 def args(parser):
     parser.add_argument("--tag", nargs = "+",
         help = "List of tags to include")
     parser.add_argument("--json", action = "store_true",
         help = "Output as json")
-    parser.add_argument("file").completer = UUIDFileCompleter
+    parser.add_argument("file", nargs = "*").completer = UUIDFileCompleter
