8b3e0e3a99beef056f916821c8cf69f9e423308e
Victorious3 <vic@nightfall.moe>
2019-05-19 16:25:13 +0200

    import can read from stdin now

--- cutespam/cli/commands/import.py
+++ cutespam/cli/commands/import.py
@@ -17,59 +17,63 @@ def main(ARGS):
     from cutespam.xmpmeta import CuteMeta
     from cutespam.config import config
     from cutespam.db import find_similar_images_hash, picture_file_for_uid
-    
-    file = Path(ARGS.file)
-    xmpfile = file.with_suffix(".xmp")
 
-    meta = CuteMeta(filename = xmpfile)
-    meta.hash = hash_img(file)
+    if ARGS.file and ARGS.file[0] == "-" and not sys.stdin.isatty():
+        ARGS.file = sys.stdin.read().splitlines()
 
-    similar = find_similar_images_hash(meta.hash, 0.9)
-    if similar:
-        print()
-        print("Found potential duplicates:")
-        for s in similar:
-            print(f"{s[0]:.1%}: {picture_file_for_uid(s[1]).resolve().as_uri() if ARGS.uri else s[1]}")
-        if not ARGS.add_duplicate and (ARGS.skip_duplicate or not yn_choice("Proceed?")): return
+    for file in ARGS.file:
+        file = Path(file)
+        if file.suffix == ".xmp": continue
+        xmpfile = file.with_suffix(".xmp")
 
-    with Image.open(file) as imgf:
-        width, height = imgf.size
-        resolution = width * height
+        meta = CuteMeta(filename = xmpfile)
+        meta.hash = hash_img(file)
 
-    print(f"Resolution: {width}x{height}")
-    print("Fetching iqdb results")
-    with open(file, "rb") as fp:
-        result = iqdb(file = fp, threshold = 0.9)
-    
-    source, data, service = upscale(result, resolution)
-    if not source:
-        if not ARGS.add_no_iqdb and (ARGS.skip_no_iqdb or not yn_choice("No relevant images found. Add anyways?")): return
-    else: print("Found image on", service)
-    
-    
-    meta.uid = uuid4()
-    meta.source = source
-    meta.date = datetime.utcnow()
+        similar = find_similar_images_hash(meta.hash, 0.9)
+        if similar:
+            print()
+            print("Found potential duplicates:")
+            for s in similar:
+                print(f"{s[0]:.1%}: {picture_file_for_uid(s[1]).resolve().as_uri() if ARGS.uri else s[1]}")
+            if not ARGS.add_duplicate and (ARGS.skip_duplicate or not yn_choice("Proceed?")): continue
 
-    if data:
-        read_meta_from_dict(meta, data)
-    
-    meta.generate_keywords()
+        with Image.open(file) as imgf:
+            width, height = imgf.size
+            resolution = width * height
 
-    print("Metadata:")
-    print(meta)
+        print(f"Resolution: {width}x{height}")
+        print("Fetching iqdb results")
+        with open(file, "rb") as fp:
+            result = iqdb(file = fp, threshold = 0.9)
+        
+        source, data, service = upscale(result, resolution)
+        if not source:
+            if not ARGS.add_no_iqdb and (ARGS.skip_no_iqdb or not yn_choice("No relevant images found. Add anyways?")): continue
+        else: print("Found image on", service)
+        
+        
+        meta.uid = uuid4()
+        meta.source = source
+        meta.date = datetime.utcnow()
 
-    meta.last_updated = datetime.utcnow()
-    meta.write()
+        if data:
+            read_meta_from_dict(meta, data)
+        
+        meta.generate_keywords()
 
-    print("Done")
-    f = shutil.move if ARGS.move else shutil.copy
-    f(str(file), str(config.image_folder / (str(meta.uid) + file.suffix)))
-    f(str(xmpfile), str(config.image_folder / (str(meta.uid) + ".xmp")))
-    
+        print("Metadata:")
+        print(meta)
+
+        meta.last_updated = datetime.utcnow()
+        meta.write()
+
+        print("Done")
+        f = shutil.move if ARGS.move else shutil.copy
+        f(str(file), str(config.image_folder / (str(meta.uid) + file.suffix)))
+        f(str(xmpfile), str(config.image_folder / (str(meta.uid) + ".xmp")))
+        
 
 def args(parser):
-    parser.add_argument("file")
     parser.add_argument("--uri", action = "store_true")
     parser.add_argument("--skip-duplicate", action = "store_true",
         help = "Skips duplicates instead of asking")
@@ -81,3 +85,5 @@ def args(parser):
         help = "Adds an image if no iqdb result was found")
     parser.add_argument("-m", "--move", action = "store_true",
         help = "If set to true, the file will be moved instead of copied")
+
+    parser.add_argument("file", nargs = "+")
